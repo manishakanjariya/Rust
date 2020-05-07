f37b958ef40e5bd6e3241501cbfd5f43dec78c89
Nikunj Kumar Nakum <40589688+nikunjkumarnakum@users.noreply.github.com>
2020-05-07 20:44:31 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// this is a line comment
+/*this is block comment,
+block comment can be nested inside of block comments.*/
+
 fn main() {
     println!("Hello, world! from Nikunj :)");
 }
+
+//above function prints a string within double quotes.
